070556612826a0d08f4c70133165d40686bb25ee
Madaleine Shields <maddiev.shields@gmail.com>
2018-10-18 11:50:26 -0700

    add validations to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,8 @@ class Product < ApplicationRecord
   belongs_to :merchant
   has_and_belongs_to_many :categories
   has_many :order_items, dependent: :destroy
-  has_many :reviews, dependent: :destroy
+  has_many :reviews, dependent: :destroy  validates :name, presence: true, uniqueness: true
+
+  validates :price, presence: true, format: { with: /\A\d+(?:\.\d{0,2})?\z/ }, numericality: { greater_than: 0, less_than: 1000000 }
+  validates_numericality_of :inventory, :only_integer => true, :greater_than_or_equal_to => 0
 end

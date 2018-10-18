a5e4847720ed04ba42e36947a0dc691cc33448c6
Amber Lynn <griffiam@gmail.com>
2018-10-18 12:53:28 -0700

    added validation tests to category model test

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -6,4 +6,28 @@ describe Category do
   it "must be valid" do
     value(category).must_be :valid?
   end
+
+  describe 'validations' do
+    before do
+      @categories = Category.new(
+        name: 'test_category_name'
+      )
+    end
+
+    it 'is valid when name is present and unique' do
+
+
+    end
+
+
+
+    it 'is invalid without a name' do
+
+
+    end
+
+    it 'is invalid with a non-unique name' do
+
+    end
+  end
 end

88b3713057996bbf8843a3c3f881d3f1764da077
Madaleine Shields <maddiev.shields@gmail.com>
2018-10-18 10:20:36 -0700

    Add relations

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,4 @@
 class Merchant < ApplicationRecord
   has_many :products
+  has_many :order_items through :product
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  belongs_to :merchants
+  has_and_belongs_to_many :categories
+  has_many :order_items, dependent: :destroy
+  has_many :reviews, dependent: :destroy
 end

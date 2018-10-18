96ff5734ea492f99fe3ca82804b6367acb785ac0
Madaleine Shields <maddiev.shields@gmail.com>
2018-10-18 10:25:28 -0700

    Edit relationships singular vs plural

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,5 @@
 class OrderItem < ApplicationRecord
   has_many :products
-  belongs_to :orders
+  belongs_to :order
+  has_one :merchant, through: :product
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  belongs_to :merchants
+  belongs_to :merchant
   has_and_belongs_to_many :categories
   has_many :order_items, dependent: :destroy
   has_many :reviews, dependent: :destroy

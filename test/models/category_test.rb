66aecf4124f998d940464410cf699e909a22aa1e
Amber Lynn <griffiam@gmail.com>
2018-10-18 13:59:53 -0700

    filled in tests for category model

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -9,25 +9,45 @@ describe Category do
 
   describe 'validations' do
     before do
-      @categories = Category.new(
+      @category = Category.new(
         name: 'test_category_name'
       )
     end
 
+
+    @CATEGORIES = %w[games toys books vitamins meditation]
+
     it 'is valid when name is present and unique' do
+      @category.name = "apparel"
 
+      result = @category.valid?
 
+      expect(result).must_equal true
+      expect( @category ).errors.messages_must_include :name
     end
 
 
 
     it 'is invalid without a name' do
+      @category.name = nil
 
+      result = @category.valid?
 
+      expect(result).wont_equal true
+      expect( @category ).errors.messages_must_include :name
     end
+
+
+    CATEGORIES = %w[games toys books vitamins meditation]
 
     it 'is invalid with a non-unique name' do
+      @category.name = CATEGORIES.first
 
+      result = @category.valid?
+
+      expect(result).wont_equal true
+      expect( @category ).errors.messages_must_include :name
     end
+
   end
 end

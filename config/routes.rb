a38d4519552e8d924e2c00c5386ccc182d82568d
Madaleine Shields <maddiev.shields@gmail.com>
2018-10-18 16:57:26 -0700

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :orders do
+  resources :orders only: [:show, :create, :update, :destroy] do
     resources :order_items, except: [:index, :show]
   end
 
+  get '/cart', to: 'orders#index', as: 'cart'
+
   resources :merchants, except: [:index] do
-    resources :products
+    resources :products, except: [:destroy]
     resources :categories, only: [:new, :create]
   end
 

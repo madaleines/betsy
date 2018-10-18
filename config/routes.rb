a1c526d1d45f35b751997923bceb5f4b768229db
Amber Lynn <griffiam@gmail.com>
2018-10-18 10:41:22 -0700

    added routes to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :order_items, except: [:index] do
+    resources :orders
+  end
+
+  resources :merchants, except: [:index] do
+    resources :products
+    resources :categories, only: [:new, :create]
+  end
+
+  resources :reviews, only: [:new, :create]
 end

dcc59b588886eb00ca73cd215db4568435fbd806
Amber Lynn <griffiam@gmail.com>
2018-10-18 14:35:12 -0700

    updated review tests by adding Arrange, Acts, and Asserts to tests

--- test/models/review_test.rb
+++ test/models/review_test.rb
@@ -8,8 +8,18 @@ describe Review do
   end
 
   describe 'relations' do
-    it "belongs to a product" do
+    before do
+      @review = Review.new(
+        product_id: 1,
+        rating: 5,
+        description: 'test-description'
+      )
+    end
 
+    it "belongs to a product" do
+      item = @review.product
+
+      expect( item ).must_be_instance_of Product
     end
 
     describe 'validations' do
@@ -21,28 +31,54 @@ describe Review do
         )
       end
 
-      it 'is valid when rating is between 1 and 5' do
+      it 'is invalid without a rating' do
+        @review.rating = nil
+        result = @review.valid?
 
-      end
-
-      it 'is valid with product_id present' do
-
+        expect( result ).wont_equal true
       end
 
       it 'is invalid if rating is not an integer' do
+        @review.rating = 'five'
 
+        result = @review.valid?
+        expect( result ).wont_equal true
       end
 
-      it 'is invalid without a rating' do
+      it 'is valid when rating is between 1 and 5' do
+        result = @review.valid?
+        expect( result ).must_equal true
+      end
 
+      it 'is valid when rating is less than 1' do
+        @review.rating = -1
+        result = @review.valid?
+
+        expect( result ).must_equal true
+      end
+
+      it 'is valid when rating is greater than 5' do
+        @review.rating = 6
+        result = @review.valid?
+
+        expect( result ).must_equal true
+      end
+
+      it 'is valid with product_id present' do
+        result = @review.valid?
+        expect( result ).must_equal true
       end
 
       it 'is valid with a description' do
-
+        result = @review.valid?
+        expect( result ).must_equal true
       end
 
       it 'is invalid without a description' do
+        @review.description = nil
+        result = @review.valid?
 
+        expect( result ).wont_equal true
       end
 
     end

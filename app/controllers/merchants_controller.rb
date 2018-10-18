3ed63797afd1281a857a4e16d528a032444ca6da
Madaleine Shields <maddiev.shields@gmail.com>
2018-10-18 11:33:33 -0700

    Begin validation tests in models

---
+++ app/controllers/merchants_controller.rb
@@ -0,0 +1,19 @@
+class MerchantsController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -6,4 +6,50 @@ describe Merchant do
   it "must be valid" do
     value(merchant).must_be :valid?
   end
+
+  describe 'relations' do
+    it 'has a product' do
+    end
+
+    it 'has some order_items through products' do
+    end
+
+  end
+
+  describe 'validations' do
+    before do
+      @merchant = Merchant.new(
+        username: 'test_merchant',
+        email: 'test_merchant@braineaze.com'
+      )
+    end
+
+    it 'is valid when username is present and unique and email is present' do
+
+
+    end
+
+    it 'is valid when email is present and unique and username is present' do
+
+
+    end
+
+    it 'is invalid without a username' do
+
+
+    end
+
+    it 'is invalid without a email' do
+
+
+    end
+
+    it 'is invalid with a non-unique username' do
+
+    end
+
+    it 'is invalid with a non-unique email' do
+
+    end
+  end
 end

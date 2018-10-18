6722812bd9afd142b5a22039965c3cded201abc4
Amber Lynn <griffiam@gmail.com>
2018-10-18 10:47:36 -0700

    adjusted routes so that order items is nested inside orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :order_items, except: [:index] do
-    resources :orders
+  resources :orders do
+    resources :order_items, except: [:index]
   end
 
   resources :merchants, except: [:index] do

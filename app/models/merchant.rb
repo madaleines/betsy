aba9adaf3187bdf215a6a0bfdd7b5427f9e7741f
Madaleine Shields <maddiev.shields@gmail.com>
2018-10-18 11:49:48 -0700

    Add validations for merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,8 @@
 class Merchant < ApplicationRecord
   has_many :products
   has_many :order_items through :product
+
+    validates :username, presence: true, uniqueness: true
+
+    validates :email, presence: true, uniqueness: true
 end

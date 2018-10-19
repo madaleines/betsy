6c4a214b5aa9ebfad4a1d7844bf9f4f3d5d0a9af
Semret Nicodimos <snicodimos@gmail.com>
2018-10-18 20:17:29 -0700

    some order tests complete and passing.

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 describe Order do
+
   describe 'relations' do
     it "has a list of order items" do
       order = orders(:one)
@@ -13,16 +14,63 @@ describe Order do
   end
 
   describe 'validations' do
-    it 'is valid with an email' do
+    before do
+      @order = Order.new(
+        email: 'aaa@gmail.com',
+        mailing_address: '12345 A Street',
+        cc: 1234123412341234,
+        cc_name: 'Aaa Aaaaaa',
+        cc_expiration: '20/22',
+        cvv: 123,
+        zip: 12345
+      )
+    end
+
+    it 'is valid when email is present and unique' do
+      is_valid = @order.valid?
+      expect( is_valid ).must_equal true
+    end
+
+    it 'is invalid when email is missing' do
+      @order.email = nil
+      is_valid = @order.valid?
+      expect( is_valid ).must_equal false
+      expect( @order.errors.messages ).must_include :email
+    end
+
+    it 'is invalid when email is non-unique' do
+      @order.email = Order.first.email
+      is_valid = @order.valid?
+      expect( is_valid ).must_equal false
+    end
+
+    it 'is valid when mailing address is present' do
+      is_valid = @order.valid?
+      expect( is_valid ).must_equal true
 
     end
 
-    it 'is valid with a mailing address' do
+    it 'is invalid when mailing address is missing' do
+      @order.mailing_address = nil
+      is_valid = @order.valid?
+      expect( is_valid ).must_equal false
+      expect( @order.errors.messages ).must_include :mailing_address
     end
 
-    it 'is valid with a cc' do
+    it 'is valid when cc is present' do
+      is_valid = @order.valid?
+      expect( is_valid ).must_equal true
+    end
+
+    it 'is invalid when cc is missing' do
+      @order.cc = nil
+      is_valid = @order.valid?
+      expect( is_valid ).must_equal false
+      expect( @order.errors.messages ).must_include :cc
 
     end
+
+
 
     it 'is valid with an integer cc number' do
 

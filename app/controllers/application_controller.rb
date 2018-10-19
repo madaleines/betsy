9922f8ae57d17eb9618548cf3bd296015bca3c25
Madaleine Shields <maddiev.shields@gmail.com>
2018-10-19 15:24:33 -0700

    Change redirect to root_path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   def require_login
     if @current_merchant.nil?
       flash[:error] = "You must be logged in to view this section"
-      redirect_to session_path
+      redirect_to root_path
     end
   end
 

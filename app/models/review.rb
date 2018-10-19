af510c2e1f8ae680c4eef4afed060acd47a5339b
Semret Nicodimos <snicodimos@gmail.com>
2018-10-18 21:50:26 -0700

    changed minor edits on review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -6,6 +6,6 @@ class Review < ApplicationRecord
   validates :rating, presence: true
   validates :rating, numericality: true
   validates :rating, numericality: { only_integer: true }
-  validates :numericality => { :less_than_or_equal_to => 5, :greater_than_or_equal_to => 1 }
+  validates :rating, :numericality => { :less_than_or_equal_to => 5, :greater_than_or_equal_to => 1 }
   validates :description, presence: true
 end

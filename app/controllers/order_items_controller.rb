9498d8cd91b34774dd444979e59836ae14588330
Amber Lynn <griffiam@gmail.com>
2018-10-19 15:56:31 -0700

    started writing controller methods and added skip_before_action

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,8 +1,24 @@
 class OrderItemsController < ApplicationController
+  skip_before_action :require_login
+
   def new
+    @order_items = (OrderItem.all).sort_by do |order_item|
+      order_item.id
+    end
   end
 
   def create
+    @order_item = OrderItem.new(order_items_params)
+    if @order_item.save
+      flash[:status] = :success
+      flash[:result_text] = "Successfully created #{@order_item.singularize} #{@order_item.id}"
+      redirect_to order_item_path(@order_item)
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "Could not create #{@order_item.singularize}"
+      flash[:messages] = @order_item.errors.messages
+      render :new, status: :bad_request
+    end
   end
 
   def edit
@@ -12,5 +28,11 @@ class OrderItemsController < ApplicationController
   end
 
   def destroy
+  end
+
+
+  private
+  def order_item_params
+    params.require(:order_item).permit(:quantity, :status, :order_id, :product_id)
   end
 end
--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,14 +1,89 @@
 require "test_helper"
 
 describe ReviewsController do
-  it "should get new" do
-    get reviews_new_url
-    value(response).must_be :success?
+  describe "new" do
+    review = Review.first
+
+    it "should get new" do
+      get new_order_order_item_path(order_item.id)
+      must_respond_with :success
+    end
   end
 
-  it "should get create" do
-    get reviews_create_url
-    value(response).must_be :success?
+  describe "create" do
+    order = Order.first
+    product = Product.first
+    review = Review.new(quantity: 5, status: 'pending', order_id: order.id, product_id: product.id)
+
+    it "succeeds for an existing order_item ID" do
+      review_count = Review.count
+
+      post order_order_items_path(order.id)
+      expect( order_item_count ).must_equal order_item_count + 1
+      must_respond_with :success
+    end
+
+    it "renders 404 not_found for a bogus order_item data" do
+
+    end
+  end
+
+  describe "edit" do
+    review = Review.first
+    id = Review.first.order_id
+
+    it "succeeds for an existing order_item ID" do
+      get edit_order_order_item_path(order_item.id, id)
+
+      must_respond_with :success
+      must_redirect_to order_order_item_path(order_item.id, id)
+    end
+
+    it "renders 404 not_found for a bogus order_item ID" do
+
+    end
+  end
+
+  describe "update" do
+    review = Review.first
+    id = Review.first.order_id
+
+    it "succeeds for valid data and an existing order_item ID" do
+      patch order_order_item_path(order_item.id, id)
+
+      must_respond_with :success
+      must_redirect_to order_path(id)
+    end
+
+    it "renders bad_request and does not update the DB for bogus data" do
+
+    end
+
+
+    it "renders bad_request for bogus data" do
+
+    end
+
+    it "renders 404 not_found for a bogus order_item ID" do
+
+    end
+
+  end
+
+  describe "delete" do
+    review = Review.first
+    id = Review.first.order_id
+
+    it "succeeds for an existing order_item ID" do
+      delete order_order_item_path(order_item.id, id)
+
+      must_respond_with :success
+      must_redirect_to order_path(id)
+    end
+
+    it "renders 404 not_found and does not delete the DB for a bogus order_item ID" do
+
+    end
   end
 
 end

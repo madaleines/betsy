cb8d9786fca883c146c9b507d75af32fe89c6ccb
Goeun Park <goeuntothepark@gmail.com>
2018-10-20 16:38:26 -0700

    add nominal and general reject tests to create in review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,12 +6,25 @@ class ReviewsController < ApplicationController
     if product
       @review = product.reviews.new
     else
-      render_404 
+      render_404
     end
-
   end
 
   def create
+    product = Product.find_by(id: params[:product_id])
+    @review = product.reviews.new(review_params)
+    
+    if @review.save
+      flash[:status] = :success
+      flash[:result_text] = "Successfully submitted review for: #{product.name}"
+      redirect_to products_path(product)
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "Could not submit review for: #{product.name}"
+      flash[:messages] = @review.errors.messages
+      render :new, status: :bad_request
+    end
+
   end
 
   private
--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -5,34 +5,91 @@ describe ReviewsController do
   let(:plushie) { products(:plushie) }
 
   describe "new" do
-    it "logged in users can get to a new review page" do
-      login(one)
-      get new_product_review_path(plushie.id)
-      must_respond_with :success
+    describe "logged in users" do
+      it "get to a new review page" do
+        login(one)
+        get new_product_review_path(plushie.id)
+        must_respond_with :success
+      end
+
+      it "can't get to a new review page for non-existent product" do
+        login(one)
+        plushie.destroy
+        get new_product_review_path(plushie.id)
+        must_respond_with :not_found
+      end
     end
 
-    it "logged in users can't get to a new review page for non-existent product" do
-      login(one)
-      plushie.destroy
-      get new_product_review_path(plushie.id)
-      must_respond_with :not_found
+    describe "guest users" do
+      it "can get to a new review page" do
+        get new_product_review_path(plushie.id)
+        must_respond_with :success
+      end
+
+      it "can't get to a new review page for non-existent product" do
+        plushie.destroy
+        get new_product_review_path(plushie.id)
+        must_respond_with :not_found
+      end
     end
 
-    it "guest users can get to a new review page" do
-      get new_product_review_path(plushie.id)
-      must_respond_with :success
-    end
-
-    it "guest users can't get to a new review page for non-existent product" do
-      plushie.destroy
-      get new_product_review_path(plushie.id)
-      must_respond_with :not_found
-    end
   end
 
   describe "create" do
+    let (:good_data) { {review: {rating: 5, product: plushie, description: 'luv it!!'} } }
+    let (:bad_data) { {review: {rating: -5, product: plushie, description: 'hate it!! :( '} } }
+    let (:bad_data2) { {review: {rating: 1, product: plushie, description: ''} } }
+
+    describe "logged in users" do
+      it "can submit review" do
+        login(one)
+        expect{
+          post product_reviews_path(plushie.id), params: good_data
+        }.must_change('Review.count', +1)
+
+        must_redirect_to products_path(plushie.id)
+      end
+
+      it "can't submit review with invalid rating" do
+        login(one)
+        expect{
+          post product_reviews_path(plushie.id), params: bad_data
+        }.wont_change('Review.count')
+        must_respond_with :bad_request
+      end
+
+      it "can't submit review with invalid description" do
+        login(one)
+        expect{
+          post product_reviews_path(plushie.id), params: bad_data2
+        }.wont_change('Review.count')
+        must_respond_with :bad_request
+      end
+    end
+
+    describe "guest users" do
+      it "guest users can submit review" do
+        expect{
+          post product_reviews_path(plushie.id), params: good_data
+        }.must_change('Review.count', +1)
+
+        must_redirect_to products_path(plushie.id)
+      end
+
+      it "can't submit review with invalid rating" do
+        expect{
+          post product_reviews_path(plushie.id), params: bad_data
+        }.wont_change('Review.count')
+        must_respond_with :bad_request
+      end
+
+      it "can't submit review with invalid description" do
+        expect{
+          post product_reviews_path(plushie.id), params: bad_data2
+        }.wont_change('Review.count')
+        must_respond_with :bad_request
+      end
+    end
 
   end
-
-
 end

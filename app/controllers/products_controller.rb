584f6357b67f2b6897c1c54f4105955e97d9f17f
Madaleine Shields <maddiev.shields@gmail.com>
2018-10-20 11:22:15 -0700

    Edit methods as needed based on tests

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,55 +1,53 @@
 class ProductsController < ApplicationController
   skip_before_action :require_login, only: [:index, :show]
 
-  before_action :require_login, only: [:new, :create]
-
-
   def index
     @products = Product.all
   end
 
   def show
     @product = Product.find_by(id: params[:id])
-    if @product.nil?
-      head :not_found
-    end
+    render_404 if @product.nil?
   end
 
   def new
-
-    @product = Product.new
+    merchant = Merchant.find_by(id: params[:merchant_id])
+    @product = merchant.products.new
   end
 
   def create
-    @product = Product.new(product_params())
-    if @product.save
+    filtered_product_params = product_params()
+    @product = Product.new(filtered_product_params)
+
+    is_successful_save = @product.save
+
+    if is_successful_save
+      flash[:success] = "Successfully created new product with title \"#{@product.name}\""
       redirect_to product_path(@product.id)
     else
+      flash.now[:error] = "Invalid product data"
       render :new, status: :bad_request
     end
   end
 
   def edit
     @product = Product.find_by(id: params[:id])
-
+    render_404 if @product.nil?
   end
 
   def update
     @product = Product.find_by(id: params[:id])
-
     if @product.update(product_params)
-      redirect_to merchant_product_path(@product.id)
+      flash[:success] = "Successfully updated product \"#{@product.name}\""
+      redirect_to product_path(@product.id)
+    else
+      flash.now[:error] = "Invalid product data"
+      render(:edit, status: :bad_request)
     end
   end
 
-  # def destroy
-  #   @product = Product.find_by(id: params[:id])
-  #   @product.destroy
-  #
-  #   redirect_to merchant_products_path
-  # end
-
   private
+
   def product_params
     return params.require(:product).permit(
       :name,
@@ -57,7 +55,7 @@ class ProductsController < ApplicationController
       :inventory,
       :merchant_id,
       :description,
-      :category_ids,
+      category_ids: [],
     )
   end
 end

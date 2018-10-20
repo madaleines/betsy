6f187328e82eb705b4480dbeb977a5d8d2ca9132
Goeun Park <goeuntothepark@gmail.com>
2018-10-19 17:03:08 -0700

    can create new category for valid merchant? in progress

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,7 +4,7 @@ require 'pry'
 describe CategoriesController do
 
   describe "logged in users" do
-    it "should get new category page for merchants that exist" do
+    it "should be able to access new category page" do
       merchant = merchants(:one)
       perform_login(merchant)
 
@@ -12,34 +12,26 @@ describe CategoriesController do
       must_respond_with :success
     end
 
+    it "should be able to create a new category with valid data" do
+      expect {
+        post categories_path, params: {category: {name: "zombie food"}}
+      }.must_change('Category.count', +1)
+
+      must_respond_with :success
+      must_redirect_to root_path
+    end
+
+
   end
 
-  # describe "new" do
-  #   it "should not get new category page for merchants that do not exist" do
-  #     merchant = merchants(:one)
-  #     merchant.products.destroy_all
-  #     merchant.categories.destroy_all
-  #
-  #     merchant.destroy
-  #     get new_merchant_category_path(merchant.id)
-  #
-  #     must_respond_with :not_found
-  #   end
-  #
-  # end
-  #
-  # describe "create" do
-  #
-  #   it "should create new category with a valid data" do
-  #     expect {
-  #       post merchant_categories_path, params: {category: {name: "zombie food"}}
-  #     }.must_change('Category.count', +1)
-  #
-  #     must_respond_with :redirect
-  #     must_redirect_to merchant_path
-  #   end
-  #
-  # end
+  describe "guest users" do
+    it "should not be able to access new category page" do
+      get new_category_path
+      must_respond_with :redirect
+      must_redirect_to root_path
+    end
+  end
+
 
 
 

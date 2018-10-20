536c56280f7e61977605ae82e533ea2f71f6210d
Amber Lynn <griffiam@gmail.com>
2018-10-20 16:08:06 -0700

    passing all test in order_item controller

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -38,28 +38,19 @@ class OrderItemsController < ApplicationController
   end
 
   def destroy
-<<<<<<< Updated upstream
-    if @current_user.id == session[:user_id]
-      @work.destroy
+    @order_item = OrderItem.find_by(id: params[:id])
 
-      flash[:success] = "Successfully destroyed work \"#{@work.title}\""
-      redirect_to works_path
-    else
-      flash[:error] = "You must be logged in as a work's user in order to delete it!"
-
-      redirect_back(fallback_location: root_path)
-    end
-=======
-    @order_item = OrderItem.find_by(order_id: params[:id])
     if @order_item.status == 'pending'
       @order_item.destroy
 
       flash[:status] = :success
       flash[:result_text] = "Successfully deleted item from cart"
-      must_redirect_to cart_path
+      redirect_to cart_path
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "Could not delete item from cart due to status being \'#{@order_item.status}\'"
+      render :new, status: :bad_request
     end
-
->>>>>>> Stashed changes
   end
 
 
--- test/controllers/order_items_controller_test.rb
+++ test/controllers/order_items_controller_test.rb
@@ -47,7 +47,6 @@ describe OrderItemsController do
 
       OrderItem.new(order_item_data[:order_item]).wont_be :valid?, "Order data wasn't invalid. Please come fix this test"
 
-      # Act
       expect {
         post order_order_items_path(order.id), params: order_item_data
       }.wont_change('OrderItem.count')
@@ -74,72 +73,37 @@ describe OrderItemsController do
       order = Order.first
       order_item = OrderItem.first
 
-
       patch order_order_item_path(order.id, order_item.id), params: {
         order_item: {quantity: 'four'}
       }
-
       must_respond_with :bad_request
     end
   end
 
   describe "delete" do
-    #   order_item = OrderItem.first
-    #   id = OrderItem.first.id
-    #
-    #   it "succeeds for an existing order_item ID" do
-    #     delete order_order_item_path(order_item.id, id)
-    #
-    #     must_respond_with :success
-    #     must_redirect_to order_path(id)
-    #   end
-    #
-    #   it "renders 404 not_found and does not delete the DB for a bogus order_item ID" do
-    #
-    #   end
-  end
-end
+    it "deletes an order item when status is pending" do
+      order_item = OrderItem.find_by(status: "pending")
+      order = order_item.order
 
-# describe "edit" do
-#   order_item = OrderItem.first
-#   id = OrderItem.first.order_id
-#
-#   it "succeeds for an existing order_item ID" do
-#     get edit_order_order_item_path(order_item.id, id)
-#
-#     must_respond_with :success
-#     must_redirect_to order_order_item_path(order_item.id, id)
-#   end
-#
-#   it "renders 404 not_found for a bogus order_item ID" do
-#
-#   end
-# end
-#
-# describe "update" do
-#   order_item = OrderItem.first
-#   id = OrderItem.first.order_id
-#
-#   it "succeeds for valid data and an existing order_item ID" do
-#     patch order_order_item_path(order_item.id, id)
-#
-#     must_respond_with :success
-#     must_redirect_to order_path(id)
-#   end
-#
-#   it "renders bad_request and does not update the DB for bogus data" do
-#
-#   end
-#
-#
-#   it "renders bad_request for bogus data" do
-#
-#   end
-#
-#   it "renders 404 not_found for a bogus order_item ID" do
-#
-#   end
-#
-# end
-#
-#
+      expect{
+        delete order_order_item_path(order.id, order_item.id)
+      }.must_change('order.order_items.count', -1)
+
+      must_respond_with :redirect
+      must_redirect_to cart_path
+    end
+
+    it "does not delete order item if status not pending" do
+      order_item = OrderItem.first
+      order = order_item.order
+
+      order_item.status = "shipped"
+
+      expect {
+        delete order_order_item_path(order.id, order_item.id)}.wont_change('order.order_items.count')
+
+        must_respond_with :bad_request
+      end
+    end
+
+  end

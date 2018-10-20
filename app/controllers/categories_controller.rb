95a7a40051505e936f35aa1df5661bc5dab9570e
Goeun Park <goeuntothepark@gmail.com>
2018-10-19 17:55:57 -0700

    working on categories controller test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,17 +7,26 @@ class CategoriesController < ApplicationController
 
   def create
     @category = Category.new(category_params)
-    #
-    # if @category.save
-    #   flash[:status] = :success
-    #   flash[:result_text] = "Successfully created new category: #{@category.name}"
-    #   redirect_to category_path(@category)
+
+    if @category.save
+      flash[:status] = :success
+      flash[:result_text] = "Successfully created new category: #{@category.name}"
+      redirect_to category_path(@category)
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "Could not create #{@category.name}"
+      flash[:messages] = @category.errors.messages
+      render :new, status: bad_request
+    end
+
     # else
     #   flash[:status] = :failure
-    #   flash[:result_text] = "Could not create #{@category.name}"
-    #   flash[:messages] = @category.errors.messages
-    #   render :new, status: bad_request
-    # end 
+    #   flash[:result_text] = "Could not create #{@media_category.singularize}"
+    #   flash[:messages] = @work.errors.messages
+    #   render :new, status: :bad_request
+    # end
+
+
 
   end
 

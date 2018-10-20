634dc66931594c2cb8473f0adf3ff32fee16f767
Goeun Park <goeuntothepark@gmail.com>
2018-10-20 15:48:39 -0700

    tests for new in reviews controller pass

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,22 @@
 class ReviewsController < ApplicationController
+  skip_before_action :require_login
+
   def new
+    product = Product.find_by(id: params[:product_id])
+    if product
+      @review = product.reviews.new
+    else
+      render_404 
+    end
+
   end
 
   def create
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :product_id, :description)
   end
 end
--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,50 +1,38 @@
 require "test_helper"
 
 describe ReviewsController do
-  describe "new" do
-    review = Review.first
-    product = Review.first.product_id
+  let(:one) { merchants(:one)}
+  let(:plushie) { products(:plushie) }
 
-    it "should get new" do
-      get new_product_reviews_path(product.id)
+  describe "new" do
+    it "logged in users can get to a new review page" do
+      login(one)
+      get new_product_review_path(plushie.id)
       must_respond_with :success
+    end
+
+    it "logged in users can't get to a new review page for non-existent product" do
+      login(one)
+      plushie.destroy
+      get new_product_review_path(plushie.id)
+      must_respond_with :not_found
+    end
+
+    it "guest users can get to a new review page" do
+      get new_product_review_path(plushie.id)
+      must_respond_with :success
+    end
+
+    it "guest users can't get to a new review page for non-existent product" do
+      plushie.destroy
+      get new_product_review_path(plushie.id)
+      must_respond_with :not_found
     end
   end
 
   describe "create" do
-    product = Product.first
-    review = Review.new(rating: 3, description: "test-description", product_id: product.id)
 
-    it "succeeds for an existing order_item ID" do
-      review_count = Review.count
-
-      post product_reviews_path(product.id)
-      expect( review_count ).must_change('Review.count' +1)
-      must_respond_with :success
-    end
-
-    it "renders 404 not_found for a bogus order_item data" do
-      must_respond_with :failure
-    end
   end
 
-
-  describe "delete" do
-    before do
-      review = Review.first
-      id = Review.first.product_id
-    end
-
-    it "succeeds for an existing order_item ID" do
-      delete product_review_path(id)
-
-      must_respond_with :success
-      must_redirect_to reviews_path
-    end
-
-    it "renders 404 not_found and does not delete the DB for a bogus order_item ID" do
-
-    end
-  end
 
 end

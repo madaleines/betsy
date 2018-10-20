2ec9c62c0db4178035de39ecde7820c909660fb6
Madaleine Shields <maddiev.shields@gmail.com>
2018-10-20 11:22:44 -0700

    Write tests to take into account guest and logged in user which pass

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,112 +1,156 @@
 require "test_helper"
-require 'pry'
 
 describe ProductsController do
   let(:one) {merchants(:one)}
   let(:bad_product_id) {Product.first.destroy.id}
 
-  it "should get index" do
-    get products_path
+  describe 'index' do
+    it "should get index for a guest" do
+      get products_path
+      must_respond_with :success
+    end
 
-    must_respond_with :success
+    it "should get index for a logged-in user" do
+      login(one)
+      get products_path
+      must_respond_with :success
+    end
   end
 
-  it "should get show" do
-    id = products(:puzzle)
+  describe 'show' do
+    it "should get show for a guest" do
+      id = products(:puzzle)
+      get product_path(id)
+      must_respond_with :success
+    end
 
-    get product_path(id)
+    it "should get show for a logged-in user" do
+      login(one)
+      id = products(:puzzle)
+      get product_path(id)
+      must_respond_with :success
+    end
 
-    must_respond_with :success
+    it "should respond with not found for showing a non-existing product for a guest" do
+      get product_path(bad_product_id)
+      must_respond_with :not_found
+    end
+
+    it "should respond with not found for showing a non-existing product for a logged-in user" do
+      login(one)
+      get product_path(bad_product_id)
+      must_respond_with :not_found
+    end
   end
 
-  it "should respond with not found for showing a non-existing product" do
-    id = products(:puzzle)
-    id.destroy
-
-    get product_path(id)
-
-    must_respond_with :not_found
+  describe 'new' do
+    it 'should get a new form to add products' do
+      login(one)
+      get new_merchant_product_path(one.id)
+      must_respond_with :success
+    end
   end
 
-  it 'should get a new form to add products' do
-    login(one)
-
-    get new_merchant_product_path(one.id)
-
-    must_respond_with :success
-  end
-
-  it 'it can create a product with valid data' do
-    login(one)
-
-
-    product_info = {
-      product: {
-        name: "some toy",
-        price: 25,
-        description: "An integlligent brain plushie",
-        inventory: 10,
-        merchant: merchants(:one),
-        category_ids: [categories(:toy).id]
+  describe 'create' do
+    it 'it can create a product with valid data' do
+      login(one)
+      product_data = {
+        product: {
+          name: "some toy",
+          price: 25,
+          inventory: 10,
+          merchant_id: one.id,
+          description: "An integlligent brain plushie",
+          category_ids: [categories(:toy).id]
+        }
       }
-    }
 
-    test_product = Product.new(product_info[:product])
-    # test_product.must_be :valid?, "Product data was invalid. fix this test"
-# binding.pry
-    valid = test_product.valid?
-    valid.must_equal true, "Product data was invalid: #{test_product.errors.messages}, please come fix this test"
+      test_product = Product.new(product_data[:product])
+      valid = test_product.valid?
+      valid.must_equal true, "Product data was invalid: #{test_product.errors.messages}, please come fix this test"
 
-    expect{
-      post(merchant_products_path(one.id), params: product_info)
-    }.must_change('Product.count', +1)
+      expect{
+        post merchant_products_path(one.id), params: product_data
+      }.must_change('Product.count', +1)
 
-    must_redirect_to product_path(Product.last)
-  end
+      must_redirect_to product_path(Product.last)
+    end
 
-  it 'does not create a new product with invalid data' do
-    login(one)
+    it 'does not create a new product with invalid data' do
+      login(one)
 
-    product_data = {
-      product: {
-        merchant: merchants(:one),
-        category_ids: [categories(:toy).id]
+      product_data = {
+        product: {
+          merchant: merchants(:one),
+          category_ids: [categories(:toy).id]
+        }
       }
-    }
 
-    Product.new(product_data[:product]).wont_be :valid?
+      Product.new(product_data[:product]).wont_be :valid?
 
-    expect {
-      post(merchant_products_path(one.id), params: product_data)
-    }.wont_change('Product.count')
+      expect {
+        post(merchant_products_path(one.id), params: product_data)
+      }.wont_change('Product.count')
 
-    must_respond_with :bad_request
+      must_respond_with :bad_request
+    end
   end
 
-  it 'responds with success for an existing product' do
-    login(one)
+  describe "edit" do
+    it "responds with success for an existing book" do
+      login(one)
+      product = Product.first
+      get edit_merchant_product_path(one.id, product.id)
+      must_respond_with :success
+    end
 
-    get edit_merchant_product_path(Product.first)
+    it "responds with not_found for a book that D.N.E." do
+      login(one)
+      get edit_merchant_product_path(one.id, bad_product_id)
+      must_respond_with :not_found
+    end
+  end
 
-    must_respond_with :success
+  describe 'update' do
+    it "updates with valid data with a logged-in user" do
+      login(one)
+
+      existing_product = products(:puddy)
+
+      put merchant_product_path(one.id, existing_product.id), params: {
+        product: {
+          name: "Brain Puddy UPDATED",
+          price: 14,
+          description: "Updated Description!",
+          inventory: 100
+        }
+      }
+
+      updated_product = Product.find_by(id: existing_product.id)
+
+      updated_product.name.must_equal "Brain Puddy UPDATED"
+      must_respond_with :redirect
+      must_redirect_to product_path(updated_product.id)
+    end
+
+    it "fails for bogus data and aproduct with a logged-in user" do
+      login(one)
+
+      existing_product = products(:puddy)
+
+      put merchant_product_path(one.id, existing_product.id), params: {
+        product: {
+          name: "",
+          price: 14,
+          description: "Updated Description!",
+          inventory: 100
+        }
+      }
+
+      unchanged_product = Product.find_by(id: existing_product.id)
+
+      unchanged_product.name.must_equal "Brain Puddy"
+      must_respond_with :bad_request
+    end
   end
 end
-
-#
-#   it "should get create" do
-#     get products_create_url
-#     value(response).must_be :success?
-#   end
-#
-#   it "should get edit" do
-#     get products_edit_url
-#     value(response).must_be :success?
-#   end
-#
-#   it "should get update" do
-#     get products_update_url
-#     value(response).must_be :success?
-#   end
-#
-#
-# end

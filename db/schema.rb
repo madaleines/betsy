39597294bf51f9cc1ccbb6cd5c37a55731c907d0
Semret Nicodimos <snicodimos@gmail.com>
2018-10-20 09:00:33 -0700

    run db:migrate to add the columnsfor status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_20_153054) do
+ActiveRecord::Schema.define(version: 2018_10_20_154111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2018_10_20_153054) do
 
   create_table "order_items", force: :cascade do |t|
     t.integer "quantity"
-    t.string "status"
+    t.string "status", default: "pending"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "product_id"

24ac36cf9273a36fe309ea41e5f7b1c56680ad82
Amber Lynn <griffiam@gmail.com>
2018-10-20 23:04:15 -0700

    wrote controller tests for merchant. since session plays heavily on creating and destroying a session, those were left to session controller and a custom method for merchants to access their custom dashboard what created

--- test/controllers/merchants_controller_test.rb
+++ test/controllers/merchants_controller_test.rb
@@ -1,34 +1,69 @@
 require "test_helper"
+require 'pry'
 
 describe MerchantsController do
-  it "should get show" do
-    get merchants_show_url
-    value(response).must_be :success?
+  describe "dashboard" do
+    it "goes to dashboard view" do
+      merchant_one = merchants(:one)
+      post dashboard_path(merchant_one.id)
+
+      #if merchant is signed it...how does test know that?
+      #if merchant is signed in, does it go to the dashboard page?
+      #if merchant is signed in, does it respond with success?
+      must_respond_with :success
+    end
+
+    it "does not render dashboard if merchant is not valid" do
+      @merchant = Merchant.first
+      @merchant.id = 0
+      #if the merchant is not a real merchant, does it return bad_request?
+      #if merchant is not a real merchant, does it redirect to root?
+      #if the merchant is not a real merchant, does it display errors(the whys?)?
+      result = @merchant.valid?
+      expect( result ).wont_equal true
+
+      expect{
+        post merchants_path(id: params[:id] )
+      }.must_respond_with :bad_request
+      # expect( @merchant.errors.messages ).must_include :id
+    end
+
+    it "does not render dashboard if merchant is not signed in" do
+      @merchant = Merchant.first
+      # what happens when merchant isn't signed-in; inside a session?
+      # if session[:user_id] != @merchant.id
+      expect{
+        post merchants_path(id: params[:id] )
+      }.must_respond_to :bad_request
+      # expect( @merchant.errors.messages ).must_include 'not logged in'
+      # end
+      #if the merchant is not signed in, does it redirect to sign-in?
+      #if the merchant is not signed in, does it display errors(the whys?)?
+    end
   end
 
-  it "should get new" do
-    get merchants_new_url
-    value(response).must_be :success?
-  end
+  describe "create" do
+    it "succeeds when all data entered is valid" do
+      skip
+      #when all data in form is correct does it give a -succeed?
+      #when it succeeds, where does the data go?
+      #when it succeeds, where does the page go?
+    end
 
-  it "should get create" do
-    get merchants_create_url
-    value(response).must_be :success?
-  end
+    it "increases the total number of merchants by 1 if data is valid" do
+      skip
+      #when merchant is created, does merchant save?
+      #when merchant is created, does Merchant.count increase by 1?
+      #when merchant is created, does new form redirect to new merchant's dashboard?
+    end
 
-  it "should get edit" do
-    get merchants_edit_url
-    value(response).must_be :success?
-  end
-
-  it "should get update" do
-    get merchants_update_url
-    value(response).must_be :success?
-  end
-
-  it "should get destroy" do
-    get merchants_destroy_url
-    value(response).must_be :success?
+    it "does not succeed when any data entered is invalid" do
+      skip
+      #when any data in form is incorrect does it give bad_request?
+      #when it does not succeed, where does the page go?
+      #when merchant is created, does Merchant.count increase by 1?
+      #when merchant is created, does new form redirect to new merchant's dashboard?
+    end
   end
 
 end

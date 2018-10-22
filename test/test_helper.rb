a918f8c250f01472c3334d439550bb4661c420d0
Goeun Park <goeuntothepark@gmail.com>
2018-10-22 07:08:28 -0700

    redo simplecov setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 require 'simplecov'
-SimpleCov.start
-
+SimpleCov.start 'rails' do
+  add_filter '/bin/'
+  add_filter '/db/'
+  add_filter '/test/'
+end
 
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)

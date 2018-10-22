6888485113721a7d5db0ff1ee84fef1f23bf949f
Madaleine Shields <maddiev.shields@gmail.com>
2018-10-22 16:26:40 -0700

    Change method name/functionality

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,19 +1,18 @@
 class Order < ApplicationRecord
   has_many :order_items
 
-  valid_cart_status = %w(cart paid complete)
+  valid_is_cart? = %w(cart paid complete)
 
   validates :status, presence: true
-  validates :email, presence: true, uniqueness: true, unless: :cart_status
-  validates :mailing_address, presence: true, unless: :cart_status
-  validates :cc, presence: true, unless: :cart_status
-  validates :cc_name, presence: true, unless: :cart_status
-  validates :cc_expiration, presence: true, unless: :cart_status
-  validates :cvv, presence: true, unless: :cart_status
-  validates :zip, presence: true, unless: :cart_status
+  validates :email, presence: true, uniqueness: true, unless: :is_cart?
+  validates :mailing_address, presence: true, unless: :is_cart?
+  validates :cc, presence: true, unless: :is_cart?
+  validates :cc_name, presence: true, unless: :is_cart?
+  validates :cc_expiration, presence: true, unless: :is_cart?
+  validates :cvv, presence: true, unless: :is_cart?
+  validates :zip, presence: true, unless: :is_cart?
 
-  def cart_status
-    return self.status != 'cart'
+  def is_cart?
+    return self.status == 'cart'
   end
-
 end

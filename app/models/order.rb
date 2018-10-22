c6d4cfb63718a48ba2bd0be9c0b8afa05beb320f
Semret Nicodimos <snicodimos@gmail.com>
2018-10-22 14:56:40 -0700

    corrected some minor syntax error fom pulling down some code

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,13 +4,13 @@ class Order < ApplicationRecord
   valid_cart_status = %w(cart paid complete)
 
   validates :status, presence: true
-  validates :email, presence: true, uniqueness: true, unless cart_status
-  validates :mailing_address, presence: true, unless cart_status
-  validates :cc, presence: true, unless cart_status
-  validates :cc_name, presence: true, unless cart_status
-  validates :cc_expiration, presence: true, unless cart_status
-  validates :cvv, presence: true, unless cart_status
-  validates :zip, presence: true, unless cart_status
+  validates :email, presence: true, uniqueness: true, unless: :cart_status
+  validates :mailing_address, presence: true, unless: :cart_status
+  validates :cc, presence: true, unless: :cart_status
+  validates :cc_name, presence: true, unless: :cart_status
+  validates :cc_expiration, presence: true, unless: :cart_status
+  validates :cvv, presence: true, unless: :cart_status
+  validates :zip, presence: true, unless: :cart_status
 
   def cart_status
     return self.status != 'cart'

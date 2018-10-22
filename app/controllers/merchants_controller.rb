777b574043f86505899e90bb37a92df14cad53bf
Amber Lynn <griffiam@gmail.com>
2018-10-22 10:23:40 -0700

    changes routes and added a show test for merchant controller

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,34 +1,27 @@
 class MerchantsController < ApplicationController
 
-  def dashboard
-    flash[:status] = :success
-    flash[:result_text] = "Successfully made it to the dashboard"
-  else
-    flash[:status] = :failure
-    flash[:result_text] = "Could not pull up your dashboard"
-    flash[:messages] = @merchant.errors.messages
-    redirect_to root_path
-  end
-end
+  def show
+    @merchant = Merchant.find_by(id: params[:id] )
 
-def show
-  @merchant = Merchant.find_by(id: params[:id] )
-  if @merchant == nil
-    flash[:status] = :failure
-    flash[:result_text] = "Merchant could not be found"
-    flash[:messages] = @merchant.errors.messages
-    redirect_to root_path
-  else
-    @products = @merchants.products
+    if @merchant.id != session[:merchant_id]
+      flash[:status] = :failure
+      flash[:result_text] = "You are not authorized to view this dashboard"
+    elsif @merchant == nil
+      flash[:status] = :failure
+      flash[:result_text] = "Merchant could not be found"
+      flash[:messages] = @merchant.errors.messages
+      redirect_to root_path
+    end
+    @products = @merchant.products
   end
-end
 
-def order_summary
-  @merchant = Merchant.find_by(id: params[:id] )
-  # if @merchant == nil
-  # flash[:status] = :failure
-  # flash[:result_text] = "Merchant could not be found"
-  # flash[:messages] = @merchant.errors.messages
-  # redirect_to root_path
-  # else
+  def order_summary
+    @merchant = Merchant.find_by(id: params[:id] )
+    # if @merchant == nil
+    # flash[:status] = :failure
+    # flash[:result_text] = "Merchant could not be found"
+    # flash[:messages] = @merchant.errors.messages
+    # redirect_to root_path
+    # else
+  end
 end

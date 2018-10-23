d7aef5502532b9082dc6b26a1413bbfc724212c1
Semret Nicodimos <snicodimos@gmail.com>
2018-10-23 15:07:37 -0700

    add shopping cart fix to view products index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,6 +7,7 @@ class ProductsController < ApplicationController
       @products = merchant.products
     else
       @products = Product.all
+      @shopping_cart = find_shopping_cart
     end
   end
 

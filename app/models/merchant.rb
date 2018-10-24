eefc3ec08c5e480c38fc84cd209f9718578c8c7e
Goeun Park <goeuntothepark@gmail.com>
2018-10-24 14:49:09 -0700

    move revenue logic from merchant controllers to model, change path name in show aka dashboard

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -8,4 +8,19 @@ class Merchant < ApplicationRecord
   def self.build_from_github(auth_hash)
     return Merchant.new(provider: auth_hash[:provider], uid: auth_hash[:uid], email: auth_hash[:info][:email], username: auth_hash[:info][:nickname])
   end
+
+  def revenue
+    revenue = 0.0
+
+    self.products.each do |product|
+      product.each do |order_item|
+        if order_item.status != 'pending'
+          revenue += order_item.product.price
+        end
+      end
+    end
+
+    return revenue
+  end
+
 end
